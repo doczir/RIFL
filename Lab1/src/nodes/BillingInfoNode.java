af894fd75d51c784a40a4e2e54d34180fe414df5
lengyela <meres@172.16.160.128>
2015-02-11 11:51:33 +0100

    BillingInfoNode implemented.
    Generator modified -> added boolean generator!

--- Lab1/src/nodes/BillingInfoNode.java
+++ Lab1/src/nodes/BillingInfoNode.java
@@ -1,28 +1,65 @@
 package nodes;
 
+import java.sql.Timestamp;
+import java.util.Random;
+
+import util.Generator;
 import model.BillingInfo;
+import model.TravelInfo;
 import nodes.TravelInfoNode.TravelInfoNodeDone;
 import channel.Channel;
 
 public class BillingInfoNode extends AbstractNode {
 
 	private BillingInfo billingInfo;
+	
+	private Random random = new Random();
+	
+	private Object lock = new Object();
+	
 
 	public BillingInfoNode(Channel channel) {
 		super(channel);
 
 		channel.add(TravelInfoNodeDone.class, msg -> {
-			gui.enable();
-			billingInfo = new BillingInfo();
-			
-			
-			gui.notify(null, billingInfo);
+			try {
+				queue.put(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+		start(()->{
+			while (true) {
+				try {
+					queue.take();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				gui.enable();
+				
+				billingInfo = new BillingInfo();
+				billingInfo.setBillingName(Generator.generateString(random, 15));
+				billingInfo.setBillingAddress(Generator.generateString(random, 15));
+				
+				gui.notify(null, billingInfo);
+				
+				synchronized (lock) {
+					try {
+						lock.wait();
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}				
+			}
 		});
 	}
 
 	@Override
 	public void next() {
-
+		synchronized (lock) {
+			lock.notify();		
+		}
 	}
 
 }
