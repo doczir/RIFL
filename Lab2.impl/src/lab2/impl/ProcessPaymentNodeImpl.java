d6e135454b8d352c857708d150dcca5bf26ef6b2
Robert Doczi <doczi.r@gmail.com>
2015-02-27 12:36:16 +0100

    Implemented select mode of recipt and process payment node, messed up something with configs so it wont run... :(

---
+++ Lab2.impl/src/lab2/impl/ProcessPaymentNodeImpl.java
@@ -0,0 +1,44 @@
+package lab2.impl;
+
+import lab2.inf.ProcessPaymentNode;
+import commonosgi.gui.GUI;
+import commonosgi.model.BillingInfo;
+import commonosgi.util.NodeBehavior;
+
+public class ProcessPaymentNodeImpl extends AbstractNode implements
+		ProcessPaymentNode {
+
+	private BillingInfo billingInfo;
+
+	public ProcessPaymentNodeImpl() {
+		super();
+		
+		new GUI("Process payment node", this);
+	}
+	
+	@Override
+	public void next() {
+		billingInfo = null;
+
+		if (!queue.isEmpty())
+			billingInfo = (BillingInfo) queue.poll();
+		else
+			gui.disable();
+	}
+
+	@Override
+	public void processPayment(BillingInfo billingInfo) {
+		if (queue.isEmpty() && this.billingInfo == null) {
+			this.billingInfo = billingInfo;
+
+			NodeBehavior.processPaymentBehavior(billingInfo);
+
+			gui.notify(null, billingInfo);
+
+			gui.enable();
+		} else {
+			queue.add(billingInfo);
+		}
+	}
+
+}
---
+++ Lab2.impl/src/lab2/impl/SelectModeOfReciptNodeImpl.java
@@ -0,0 +1,105 @@
+package lab2.impl;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import lab2.inf.DeliveryAddressNode;
+import lab2.inf.SelectModeOfReciptNode;
+import commonosgi.gui.GUI;
+import commonosgi.gui.SMORGUI;
+import commonosgi.model.BillingInfo;
+
+public class SelectModeOfReciptNodeImpl extends AbstractNode implements SelectModeOfReciptNode {
+
+	private BillingInfo billingInfo;
+	private boolean delivery;
+	private DeliveryAddressNode dan;
+	
+	public SelectModeOfReciptNodeImpl() {
+		super();
+		
+		new SMORGUI("Select mode of recipt", this);
+	}
+	
+	
+	
+	@Override
+	public void setGui(GUI gui) {
+		super.setGui(gui);
+		
+		JButton receiptInPersonalBtn = ((SMORGUI) gui).getReceiptInPersonalBtn();
+		JButton receiptDeliveryBtn = ((SMORGUI) gui).getReceiptDeliveryBtn();
+		
+		receiptInPersonalBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setReceiptInPersonal();
+			}
+		});
+		
+		receiptDeliveryBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setReceiptDelivery();
+			}
+		});
+	}
+
+	public void setReceiptDelivery() {
+		if (billingInfo != null) {
+			delivery = true;
+			
+			billingInfo.setDeliveryAddress("-->");
+			
+			gui.notify(null, billingInfo);
+		}
+	}
+	
+	public void setReceiptInPersonal() {
+		if (billingInfo != null) {
+			delivery = false;
+			
+			billingInfo.setDeliveryAddress("-");
+			
+			gui.notify(null, billingInfo);
+		}
+	}
+
+	@Override
+	public void next() {
+		if(delivery)
+			dan.getDeliveryAddress(billingInfo);
+		billingInfo = null;
+		
+		if(!queue.isEmpty()) 
+			billingInfo = (BillingInfo) queue.poll();
+		else
+			gui.disable();
+	}
+
+	@Override
+	public void selectModeOfRecipt(BillingInfo billingInfo) {
+		if (queue.isEmpty() && billingInfo == null) {
+			this.billingInfo = billingInfo;
+			
+			gui.notify(null, billingInfo);
+			gui.enable();
+		} else {
+			queue.add(billingInfo);
+		}
+	}
+	
+	public void setDAN(DeliveryAddressNode dan) {
+		this.dan = dan;		
+	}
+	
+	public void unsetDAN(DeliveryAddressNode dan) {
+		if(this.dan == dan)
+			this.dan = null;
+	}
+
+}
